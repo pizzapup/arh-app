a3fafc7be6adcc85c6d7cce892573cfa80374fd3
pizzapup <57334884+pizzapup@users.noreply.github.com>
2022-03-05 11:13:49 -0500

    updated 3-5-22

--- src/components/CreatePup/CreatePup.js
+++ src/components/CreatePup/CreatePup.js
@@ -3,10 +3,6 @@ import React, { useState } from "react";
 import firebase from "../../utilities/firebase";
 import DogIcon from "../../images/dog";
 import "./CreatePup.css";
-import { SwatchesPicker } from "react-color";
-import ReactColorPicker from "@radial-color-picker/react-color-picker";
-import "@radial-color-picker/react-color-picker/dist/react-color-picker.min.css";
-
 import {
   InputGroup,
   Col,
@@ -14,25 +10,18 @@ import {
   Container,
   Card,
   Button,
+  FormControl,
+  FloatingLabel,
 } from "react-bootstrap";
-import ColorPick from "./ColorPick";
+
 function CreatePup() {
   const [name, setName] = useState("");
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
-  // const [color, setColor] = useState("");
-  const [color, setColor] = React.useState({
-    hue: 200,
-    saturation: 100,
-    luminosity: 50,
-    alpha: 1,
-  });
+  const [color, setColor] = useState("#10b56c");
   const handleColorChange = (e) => {
     setColor(e.target.value);
-  };
-  const onInput = (hue) => {
-    setColor({ ...color, hue });
   };
 
   const background = "default-background";
@@ -51,40 +40,38 @@ function CreatePup() {
     <div className="create-pup-container">
       <div className="create-pup">
         <div className="form-side">
-          <div>Name this dog:</div>
-          <label className="name-input-label">
-            <input
-              className="custom-input-bite custom-input-text"
-              type="text"
-              placeholder="Name this dog:"
-              aria-label="Enter a name for this dog"
-              onChange={handleNameChange}
-              value={name}
-            />
-          </label>
-          <div className="color-input-div">
-            <label className="color-input-label">
-              {/* <input
-                className="custom-input-bite "
+          <Card.Title>Name this dog:</Card.Title>
+          <Card.Body>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">name:</InputGroup.Text>
+              <FormControl
+                onChange={handleNameChange}
+                value={name}
+                placeholder="dog name"
+                aria-label="dog name"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">color:</InputGroup.Text>
+              <FormControl
                 type="color"
-                placeholder="Pick a color for this dog"
-                aria-label="Pick a color for this dog"
                 onChange={handleColorChange}
                 value={color}
-              /> */}
-              <ReactColorPicker {...color} onInput={onInput} />
-            </label>
-          </div>
-          <div className="form-button-div">
-            <Button bsPrefix="btn-custom-dark" onClick={createPupObj}>
+                placeholder="dog color"
+                aria-label="dog color"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <Button bsPrefix="btn-custom-dark mb-3" onClick={createPupObj}>
               Add to list
             </Button>
-          </div>
+          </Card.Body>
         </div>
-        <Card style={{ width: "18rem" }}>
+        <Container style={{ width: "18rem" }}>
           <h2 className="form-preview-title">{name}</h2>
           <DogIcon fill={color} className="dog-icon-preview floating" />
-        </Card>
+        </Container>
       </div>
     </div>
   );
--- src/components/PupTravel/PupTravel.js
+++ src/components/PupTravel/PupTravel.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import { React, useState } from "react";
 import firebase from "../../utilities/firebase";
 import "../PupCard/PupCard.css";
-import { Button, Card } from "react-bootstrap";
+import { Button, Modal, Card } from "react-bootstrap";
 
 function PupTravel({ pup }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const goHere = (e) => {
     const pupRef = firebase.database().ref("name-this-dog").child(pup.id);
     pupRef.update({
@@ -16,24 +20,40 @@ function PupTravel({ pup }) {
     pupRef.remove();
   };
   return (
-    <Card className="PupTravel card-travel-section">
+    <div className="PupTravel">
       <Button
+        disable
         size="sm"
-        className="pup-button travel-button"
-        onClick={goHere}
+        className="btn-sm "
+        bsPrefix="  btn-custom-dark btn-travel"
+        // onClick={goHere}
+        onClick={handleShow}
         value="somewhereDefault"
       >
-        Go Here
+        <span className="btn-text">go here</span>
       </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>coming soon!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>check back soon for more features!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Button
         variant="secondary"
         size="sm"
-        className="pup-button travel-button"
+        className="btn-sm"
+        bsPrefix="btn-custom-dark btn-travel"
         onClick={deletePup}
       >
-        Go Home!
+        <span className="btn-text">go home!</span>
       </Button>
-    </Card>
+    </div>
   );
 }
 export default PupTravel;
